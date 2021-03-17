b1738caebd0d35aa6591bd805c05f058dc8fb188
Tom Hoey <thoey@academytrainee.com>
2021-03-17 10:49:22 +0000

    TDAP-10 #comment Added readALL functionality for To Do List.

---
+++ src/test/java/com/qa/toDoList/data/CustomerRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.qa.toDoList.data;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.qa.toDoList.data.respository.CustomerRepository;
+
+@DataJpaTest
+public class CustomerRepositoryTest {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	private EntityManager entityManager;
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	@Test
+	void injectedComponentsAreNotNull() {
+		assertThat(dataSource).isNotNull();
+	    assertThat(jdbcTemplate).isNotNull();
+	    assertThat(entityManager).isNotNull();
+	    assertThat(customerRepository).isNotNull();
+	}
+
+}
